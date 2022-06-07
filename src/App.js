3f5ed7908815164af234f27facd5f973f4396cbb
rba1aji <rjibalaji@gmail.com>
2022-06-07 21:32:33 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import "./style.css";
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>H   llo Stack  Blitz!</h1>
+      <p>Start   iting to see some magic happen :)</p>
     </div>
   );
 }
