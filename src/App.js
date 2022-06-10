c1292e3c63253c27b02d5cc87d500af96fa731da
rba1aji <rjibalaji@gmail.com>
2022-06-10 20:39:39 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import "./style.css";
+import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
 
 export default function App() {
   return (
-    <div>
-      <h1>H   llo Stack  Blitz!</h1>
-      <p>Start   iting to see some magic happen :)</p>
-    </div>
+    <>
+
+    </>
   );
 }
