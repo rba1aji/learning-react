9de2a8b39d35b89975b4de19f73538f58c4c2951
rba1aji <rjibalaji@gmail.com>
2022-06-10 20:46:44 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
-import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
+const Nav = () => {
+  return (
+    <>
+      <Link to="\knapsack">Knapsack</Link>
+    </>
+  );
+};
 
 export default function App() {
   return (
     <>
-
+      <Nav />
+      <Router>
+        <Switch>
+          <Route path="/knapsack" exact element="<Nav />" />
+        </Switch>
+      </Router>
     </>
   );
 }
