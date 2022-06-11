61cbe707084ee71f7bdc4a411024b3ee5e34e517
rba1aji <rjibalaji@gmail.com>
2022-06-11 15:42:24 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Outlet, Link } from 'react-router-dom'
-import Knapsack from './components/Knapsack'
-import Floyds from './components/Floyds'
-import Warshalls from './components/Warshalls'
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+  Link,
+} from 'react-router-dom';
+import Knapsack from './components/Knapsack';
+import Floyds from './components/Floyds';
+import Warshalls from './components/Warshalls';
 // import Knapsack from './pages/Knapsack';
 
 function Nav() {
@@ -10,9 +16,11 @@ function Nav() {
     <>
       {/* hey */}
       <p>
-        <a href="/knapsack">Knapsack </a><br/>
-        <a href="/floyds">Floyds </a><br/>
-        <a href="/warshalls">warshalls </a>
+        <Link to="/knapsack">Knapsack </Link>
+        <br />
+        <Link to="/floyds">Floyds </Link>
+        <br />
+        <Link to="/warshalls">warshalls </Link>
       </p>
       <Outlet />
     </>
@@ -25,12 +33,12 @@ export default function App() {
       {/* <Nav /> */}
       <Router>
         <Routes>
-           <Route path="/" element={<Nav/>} >
-             <Route index element={<Nav/>}/>
-           <Route path="knapsack" element={<Knapsack/>} />
-           <Route path="floyds" element={<Floyds/>} />
-           <Route path="warshalls" element={<Warshalls/>} />
-           </Route>
+          <Route path="/" element={<Nav />}>
+            <Route index element={<Nav />} />
+            <Route path="knapsack" element={<Knapsack />} />
+            <Route path="floyds" element={<Floyds />} />
+            <Route path="warshalls" element={<Warshalls />} />
+          </Route>
         </Routes>
       </Router>
     </>
