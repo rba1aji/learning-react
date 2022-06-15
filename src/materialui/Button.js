b955216a15808b9c89b8c66dc89b6d91a302f28c
rba1aji <rjibalaji@gmail.com>
2022-06-15 20:43:03 +0530

    1 file modified

--- src/materialui/Button.js
+++ src/materialui/Button.js
@@ -1,6 +1,6 @@
+import React from 'react';
 import Button from '@mui/material/Button';
-export default function bt1(){
-  return(
-    <Button variant="contained">Hello World</Button>
-  );
+// import Button from 'material-ui'
+export default function bt1() {
+  return <Button variant="contained">Hello World</Button>;
 }
