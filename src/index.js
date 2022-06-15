2794ed3840e692b41f56095643901324022fb454
rba1aji <rjibalaji@gmail.com>
2022-06-15 20:09:14 +0530

    1 file modified, 1 added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
+import Button from './materialui/Button'
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
@@ -9,5 +10,6 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <App />
+    <Button />
   </StrictMode>
 );
